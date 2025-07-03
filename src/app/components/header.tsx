5185bc1e399755b03d4333be5e110c8f888b0573
GEUNNN <geunhwa.lee.01@gmail.com>
2025-07-03 22:35:16 +0900

    feat: Header 추가 및 레이아웃에 적용

---
+++ src/app/components/header.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Header() {
+  const pathname = usePathname();
+  const isActive = (path: string) => pathname === path;
+  const isActiveStyle = "underline underline-offset-8 decoration-[#4880EE]";
+
+  return (
+    <header className="bg-background text-foreground h-20 flex items-center">
+      <h1 className="pl-40 py-4 font-bold text-2xl flex-none">
+        CERTICOS BOOKS
+      </h1>
+      <div className="pl-100 flex gap-14">
+        {menuItems.map((item) => (
+          <Link
+            key={item.path}
+            href={item.path}
+            className={`text-xl font-medium ${
+              isActive(item.path) ? isActiveStyle : ""
+            }`}
+          >
+            {item.label}
+          </Link>
+        ))}
+      </div>
+      <div className="flex-none min-w-20" />
+    </header>
+  );
+}
+
+const menuItems = [
+  {
+    label: "도서 검색",
+    path: "/",
+  },
+  {
+    label: "내가 찜한 책",
+    path: "/wishlist",
+  },
+];
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
+import Header from "./components/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const notoSansKR = Noto_Sans_KR({
+  variable: "--font-noto-sans-kr",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["500", "700"],
 });
 
 export const metadata: Metadata = {
@@ -24,10 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${notoSansKR.variable} antialiased`}>
+        <Header />
+        <main>{children}</main>
       </body>
     </html>
   );
