64c02d8c50ce6ead3c79f600b14d90c2141419f6
GEUNNN <geunhwa.lee.01@gmail.com>
2025-07-06 15:28:02 +0900

    feat: bookListItem 레이아웃 추가

--- src/components/BookListItem.tsx
+++ src/components/BookListItem.tsx
@@ -11,13 +11,17 @@ const BookListItem = ({ book }: BookListItemProps) => {
     <article className="flex flex-row justify-between p-4 border-b border-[#D2D6DA]">
       <div className="flex items-center pl-8 gap-12">
         <Image src={book.thumbnail} alt={book.title} width={48} height={68} />
-        <div className="flex flex-row gap-1">
-          <div>{book.title}</div>
-          <div>{book.authors}</div>
+        <div className="flex flex-row items-center gap-4">
+          <div className="text-lg font-bold text-[#353C49]">{book.title}</div>
+          <div className="font-medium text-sm text-[#6D7582]">
+            {book.authors}
+          </div>
         </div>
       </div>
-      <div className="flex gap-2 items-center">
-        <span>{book.sale_price.toLocaleString()}원</span>
+      <div className="flex gap-14 items-center">
+        <span className="text-lg font-bold text-[#353C49]">
+          {book.sale_price.toLocaleString()}원
+        </span>
         <div className="flex flex-row gap-2">
           <Button variant="primary" />
           <Button variant="secondary" />
